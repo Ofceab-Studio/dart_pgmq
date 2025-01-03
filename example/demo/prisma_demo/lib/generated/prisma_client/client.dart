08239bc726e1ecfa08906de0c6ea9f996cb59b92
yayahc <hamedcuenca5@gmail.com>
2025-01-03 18:43:17 +0000

    chore: move prisma demo into demo folder

---
+++ example/demo/prisma_demo/lib/generated/prisma_client/client.dart
@@ -0,0 +1,575 @@
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:orm/dmmf.dart' as _i4;
+import 'package:orm/engines/binary.dart' as _i5;
+import 'package:orm/orm.dart' as _i1;
+
+import 'model.dart' as _i2;
+import 'prisma.dart' as _i3;
+
+class UserDelegate {
+  const UserDelegate._(this._client);
+
+  final PrismaClient _client;
+
+  _i1.ActionClient<_i2.User?> findUnique({
+    required _i3.UserWhereUniqueInput where,
+    _i3.UserSelect? select,
+  }) {
+    final args = {
+      'where': where,
+      'select': select,
+    };
+    final query = _i1.serializeJsonQuery(
+      args: args,
+      modelName: 'User',
+      action: _i1.JsonQueryAction.findUnique,
+      datamodel: PrismaClient.datamodel,
+    );
+    final result = _client.$engine.request(
+      query,
+      headers: _client.$transaction.headers,
+      transaction: _client.$transaction.transaction,
+    );
+    return _i1.ActionClient<_i2.User?>(
+      action: 'findUniqueUser',
+      result: result,
+      factory: (e) => e != null ? _i2.User.fromJson(e) : null,
+    );
+  }
+
+  _i1.ActionClient<_i2.User> findUniqueOrThrow({
+    required _i3.UserWhereUniqueInput where,
+    _i3.UserSelect? select,
+  }) {
+    final args = {
+      'where': where,
+      'select': select,
+    };
+    final query = _i1.serializeJsonQuery(
+      args: args,
+      modelName: 'User',
+      action: _i1.JsonQueryAction.findUniqueOrThrow,
+      datamodel: PrismaClient.datamodel,
+    );
+    final result = _client.$engine.request(
+      query,
+      headers: _client.$transaction.headers,
+      transaction: _client.$transaction.transaction,
+    );
+    return _i1.ActionClient<_i2.User>(
+      action: 'findUniqueUserOrThrow',
+      result: result,
+      factory: (e) => _i2.User.fromJson(e),
+    );
+  }
+
+  _i1.ActionClient<_i2.User?> findFirst({
+    _i3.UserWhereInput? where,
+    _i1.PrismaUnion<Iterable<_i3.UserOrderByWithRelationInput>,
+            _i3.UserOrderByWithRelationInput>?
+        orderBy,
+    _i3.UserWhereUniqueInput? cursor,
+    int? take,
+    int? skip,
+    _i1.PrismaUnion<_i3.UserScalar, Iterable<_i3.UserScalar>>? distinct,
+    _i3.UserSelect? select,
+  }) {
+    final args = {
+      'where': where,
+      'orderBy': orderBy,
+      'cursor': cursor,
+      'take': take,
+      'skip': skip,
+      'distinct': distinct,
+      'select': select,
+    };
+    final query = _i1.serializeJsonQuery(
+      args: args,
+      modelName: 'User',
+      action: _i1.JsonQueryAction.findFirst,
+      datamodel: PrismaClient.datamodel,
+    );
+    final result = _client.$engine.request(
+      query,
+      headers: _client.$transaction.headers,
+      transaction: _client.$transaction.transaction,
+    );
+    return _i1.ActionClient<_i2.User?>(
+      action: 'findFirstUser',
+      result: result,
+      factory: (e) => e != null ? _i2.User.fromJson(e) : null,
+    );
+  }
+
+  _i1.ActionClient<_i2.User> findFirstOrThrow({
+    _i3.UserWhereInput? where,
+    _i1.PrismaUnion<Iterable<_i3.UserOrderByWithRelationInput>,
+            _i3.UserOrderByWithRelationInput>?
+        orderBy,
+    _i3.UserWhereUniqueInput? cursor,
+    int? take,
+    int? skip,
+    _i1.PrismaUnion<_i3.UserScalar, Iterable<_i3.UserScalar>>? distinct,
+    _i3.UserSelect? select,
+  }) {
+    final args = {
+      'where': where,
+      'orderBy': orderBy,
+      'cursor': cursor,
+      'take': take,
+      'skip': skip,
+      'distinct': distinct,
+      'select': select,
+    };
+    final query = _i1.serializeJsonQuery(
+      args: args,
+      modelName: 'User',
+      action: _i1.JsonQueryAction.findFirstOrThrow,
+      datamodel: PrismaClient.datamodel,
+    );
+    final result = _client.$engine.request(
+      query,
+      headers: _client.$transaction.headers,
+      transaction: _client.$transaction.transaction,
+    );
+    return _i1.ActionClient<_i2.User>(
+      action: 'findFirstUserOrThrow',
+      result: result,
+      factory: (e) => _i2.User.fromJson(e),
+    );
+  }
+
+  _i1.ActionClient<Iterable<_i2.User>> findMany({
+    _i3.UserWhereInput? where,
+    _i1.PrismaUnion<Iterable<_i3.UserOrderByWithRelationInput>,
+            _i3.UserOrderByWithRelationInput>?
+        orderBy,
+    _i3.UserWhereUniqueInput? cursor,
+    int? take,
+    int? skip,
+    _i1.PrismaUnion<_i3.UserScalar, Iterable<_i3.UserScalar>>? distinct,
+    _i3.UserSelect? select,
+  }) {
+    final args = {
+      'where': where,
+      'orderBy': orderBy,
+      'cursor': cursor,
+      'take': take,
+      'skip': skip,
+      'distinct': distinct,
+      'select': select,
+    };
+    final query = _i1.serializeJsonQuery(
+      args: args,
+      modelName: 'User',
+      action: _i1.JsonQueryAction.findMany,
+      datamodel: PrismaClient.datamodel,
+    );
+    final result = _client.$engine.request(
+      query,
+      headers: _client.$transaction.headers,
+      transaction: _client.$transaction.transaction,
+    );
+    return _i1.ActionClient<Iterable<_i2.User>>(
+      action: 'findManyUser',
+      result: result,
+      factory: (values) =>
+          (values as Iterable).map((e) => _i2.User.fromJson(e)),
+    );
+  }
+
+  _i1.ActionClient<_i2.User> create({
+    _i1.PrismaUnion<_i3.UserCreateInput, _i3.UserUncheckedCreateInput>? data,
+    _i3.UserSelect? select,
+  }) {
+    final args = {
+      'data': data,
+      'select': select,
+    };
+    final query = _i1.serializeJsonQuery(
+      args: args,
+      modelName: 'User',
+      action: _i1.JsonQueryAction.createOne,
+      datamodel: PrismaClient.datamodel,
+    );
+    final result = _client.$engine.request(
+      query,
+      headers: _client.$transaction.headers,
+      transaction: _client.$transaction.transaction,
+    );
+    return _i1.ActionClient<_i2.User>(
+      action: 'createOneUser',
+      result: result,
+      factory: (e) => _i2.User.fromJson(e),
+    );
+  }
+
+  _i1.ActionClient<_i3.AffectedRowsOutput> createMany({
+    required _i1
+        .PrismaUnion<_i3.UserCreateManyInput, Iterable<_i3.UserCreateManyInput>>
+        data,
+    bool? skipDuplicates,
+  }) {
+    final args = {
+      'data': data,
+      'skipDuplicates': skipDuplicates,
+    };
+    final query = _i1.serializeJsonQuery(
+      args: args,
+      modelName: 'User',
+      action: _i1.JsonQueryAction.createMany,
+      datamodel: PrismaClient.datamodel,
+    );
+    final result = _client.$engine.request(
+      query,
+      headers: _client.$transaction.headers,
+      transaction: _client.$transaction.transaction,
+    );
+    return _i1.ActionClient<_i3.AffectedRowsOutput>(
+      action: 'createManyUser',
+      result: result,
+      factory: (e) => _i3.AffectedRowsOutput.fromJson(e),
+    );
+  }
+
+  _i1.ActionClient<Iterable<_i2.CreateManyUserAndReturnOutputType>>
+      createManyAndReturn({
+    required _i1
+        .PrismaUnion<_i3.UserCreateManyInput, Iterable<_i3.UserCreateManyInput>>
+        data,
+    bool? skipDuplicates,
+    _i3.CreateManyUserAndReturnOutputTypeSelect? select,
+  }) {
+    final args = {
+      'data': data,
+      'skipDuplicates': skipDuplicates,
+      'select': select,
+    };
+    final query = _i1.serializeJsonQuery(
+      args: args,
+      modelName: 'User',
+      action: _i1.JsonQueryAction.createManyAndReturn,
+      datamodel: PrismaClient.datamodel,
+    );
+    final result = _client.$engine.request(
+      query,
+      headers: _client.$transaction.headers,
+      transaction: _client.$transaction.transaction,
+    );
+    return _i1.ActionClient<Iterable<_i2.CreateManyUserAndReturnOutputType>>(
+      action: 'createManyUserAndReturn',
+      result: result,
+      factory: (values) => (values as Iterable)
+          .map((e) => _i2.CreateManyUserAndReturnOutputType.fromJson(e)),
+    );
+  }
+
+  _i1.ActionClient<_i2.User?> update({
+    required _i1.PrismaUnion<_i3.UserUpdateInput, _i3.UserUncheckedUpdateInput>
+        data,
+    required _i3.UserWhereUniqueInput where,
+    _i3.UserSelect? select,
+  }) {
+    final args = {
+      'data': data,
+      'where': where,
+      'select': select,
+    };
+    final query = _i1.serializeJsonQuery(
+      args: args,
+      modelName: 'User',
+      action: _i1.JsonQueryAction.updateOne,
+      datamodel: PrismaClient.datamodel,
+    );
+    final result = _client.$engine.request(
+      query,
+      headers: _client.$transaction.headers,
+      transaction: _client.$transaction.transaction,
+    );
+    return _i1.ActionClient<_i2.User?>(
+      action: 'updateOneUser',
+      result: result,
+      factory: (e) => e != null ? _i2.User.fromJson(e) : null,
+    );
+  }
+
+  _i1.ActionClient<_i3.AffectedRowsOutput> updateMany({
+    required _i1.PrismaUnion<_i3.UserUpdateManyMutationInput,
+            _i3.UserUncheckedUpdateManyInput>
+        data,
+    _i3.UserWhereInput? where,
+  }) {
+    final args = {
+      'data': data,
+      'where': where,
+    };
+    final query = _i1.serializeJsonQuery(
+      args: args,
+      modelName: 'User',
+      action: _i1.JsonQueryAction.updateMany,
+      datamodel: PrismaClient.datamodel,
+    );
+    final result = _client.$engine.request(
+      query,
+      headers: _client.$transaction.headers,
+      transaction: _client.$transaction.transaction,
+    );
+    return _i1.ActionClient<_i3.AffectedRowsOutput>(
+      action: 'updateManyUser',
+      result: result,
+      factory: (e) => _i3.AffectedRowsOutput.fromJson(e),
+    );
+  }
+
+  _i1.ActionClient<_i2.User> upsert({
+    required _i3.UserWhereUniqueInput where,
+    required _i1.PrismaUnion<_i3.UserCreateInput, _i3.UserUncheckedCreateInput>
+        create,
+    required _i1.PrismaUnion<_i3.UserUpdateInput, _i3.UserUncheckedUpdateInput>
+        update,
+    _i3.UserSelect? select,
+  }) {
+    final args = {
+      'where': where,
+      'create': create,
+      'update': update,
+      'select': select,
+    };
+    final query = _i1.serializeJsonQuery(
+      args: args,
+      modelName: 'User',
+      action: _i1.JsonQueryAction.upsertOne,
+      datamodel: PrismaClient.datamodel,
+    );
+    final result = _client.$engine.request(
+      query,
+      headers: _client.$transaction.headers,
+      transaction: _client.$transaction.transaction,
+    );
+    return _i1.ActionClient<_i2.User>(
+      action: 'upsertOneUser',
+      result: result,
+      factory: (e) => _i2.User.fromJson(e),
+    );
+  }
+
+  _i1.ActionClient<_i2.User?> delete({
+    required _i3.UserWhereUniqueInput where,
+    _i3.UserSelect? select,
+  }) {
+    final args = {
+      'where': where,
+      'select': select,
+    };
+    final query = _i1.serializeJsonQuery(
+      args: args,
+      modelName: 'User',
+      action: _i1.JsonQueryAction.deleteOne,
+      datamodel: PrismaClient.datamodel,
+    );
+    final result = _client.$engine.request(
+      query,
+      headers: _client.$transaction.headers,
+      transaction: _client.$transaction.transaction,
+    );
+    return _i1.ActionClient<_i2.User?>(
+      action: 'deleteOneUser',
+      result: result,
+      factory: (e) => e != null ? _i2.User.fromJson(e) : null,
+    );
+  }
+
+  _i1.ActionClient<_i3.AffectedRowsOutput> deleteMany(
+      {_i3.UserWhereInput? where}) {
+    final args = {'where': where};
+    final query = _i1.serializeJsonQuery(
+      args: args,
+      modelName: 'User',
+      action: _i1.JsonQueryAction.deleteMany,
+      datamodel: PrismaClient.datamodel,
+    );
+    final result = _client.$engine.request(
+      query,
+      headers: _client.$transaction.headers,
+      transaction: _client.$transaction.transaction,
+    );
+    return _i1.ActionClient<_i3.AffectedRowsOutput>(
+      action: 'deleteManyUser',
+      result: result,
+      factory: (e) => _i3.AffectedRowsOutput.fromJson(e),
+    );
+  }
+
+  _i1.ActionClient<Iterable<_i3.UserGroupByOutputType>> groupBy({
+    _i3.UserWhereInput? where,
+    _i1.PrismaUnion<Iterable<_i3.UserOrderByWithAggregationInput>,
+            _i3.UserOrderByWithAggregationInput>?
+        orderBy,
+    required _i1.PrismaUnion<Iterable<_i3.UserScalar>, _i3.UserScalar> by,
+    _i3.UserScalarWhereWithAggregatesInput? having,
+    int? take,
+    int? skip,
+    _i3.UserGroupByOutputTypeSelect? select,
+  }) {
+    final args = {
+      'where': where,
+      'orderBy': orderBy,
+      'by': _i1.JsonQuery.groupBySerializer(by),
+      'having': having,
+      'take': take,
+      'skip': skip,
+      'select': select ?? _i1.JsonQuery.groupBySelectSerializer(by),
+    };
+    final query = _i1.serializeJsonQuery(
+      args: args,
+      modelName: 'User',
+      action: _i1.JsonQueryAction.groupBy,
+      datamodel: PrismaClient.datamodel,
+    );
+    final result = _client.$engine.request(
+      query,
+      headers: _client.$transaction.headers,
+      transaction: _client.$transaction.transaction,
+    );
+    return _i1.ActionClient<Iterable<_i3.UserGroupByOutputType>>(
+      action: 'groupByUser',
+      result: result,
+      factory: (values) => (values as Iterable)
+          .map((e) => _i3.UserGroupByOutputType.fromJson(e)),
+    );
+  }
+
+  _i1.ActionClient<_i3.AggregateUser> aggregate({
+    _i3.UserWhereInput? where,
+    _i1.PrismaUnion<Iterable<_i3.UserOrderByWithRelationInput>,
+            _i3.UserOrderByWithRelationInput>?
+        orderBy,
+    _i3.UserWhereUniqueInput? cursor,
+    int? take,
+    int? skip,
+    _i3.AggregateUserSelect? select,
+  }) {
+    final args = {
+      'where': where,
+      'orderBy': orderBy,
+      'cursor': cursor,
+      'take': take,
+      'skip': skip,
+      'select': select,
+    };
+    final query = _i1.serializeJsonQuery(
+      args: args,
+      modelName: 'User',
+      action: _i1.JsonQueryAction.aggregate,
+      datamodel: PrismaClient.datamodel,
+    );
+    final result = _client.$engine.request(
+      query,
+      headers: _client.$transaction.headers,
+      transaction: _client.$transaction.transaction,
+    );
+    return _i1.ActionClient<_i3.AggregateUser>(
+      action: 'aggregateUser',
+      result: result,
+      factory: (e) => _i3.AggregateUser.fromJson(e),
+    );
+  }
+}
+
+class PrismaClient extends _i1.BasePrismaClient<PrismaClient> {
+  PrismaClient({
+    super.datasourceUrl,
+    super.datasources,
+    super.errorFormat,
+    super.log,
+    _i1.Engine? engine,
+  }) : _engine = engine;
+
+  static final datamodel = _i4.DataModel.fromJson({
+    'enums': [],
+    'models': [
+      {
+        'name': 'User',
+        'dbName': null,
+        'schema': null,
+        'fields': [
+          {
+            'name': 'id',
+            'kind': 'scalar',
+            'isList': false,
+            'isRequired': true,
+            'isUnique': false,
+            'isId': true,
+            'isReadOnly': false,
+            'hasDefaultValue': true,
+            'type': 'Int',
+            'nativeType': null,
+            'default': {
+              'name': 'autoincrement',
+              'args': [],
+            },
+            'isGenerated': false,
+            'isUpdatedAt': false,
+          }
+        ],
+        'primaryKey': null,
+        'uniqueFields': [],
+        'uniqueIndexes': [],
+        'isGenerated': false,
+      }
+    ],
+    'types': [],
+    'indexes': [
+      {
+        'model': 'User',
+        'type': 'id',
+        'isDefinedOnField': true,
+        'fields': [
+          {'name': 'id'}
+        ],
+      }
+    ],
+  });
+
+  _i1.Engine? _engine;
+
+  _i1.TransactionClient<PrismaClient>? _transaction;
+
+  @override
+  get $transaction {
+    if (_transaction != null) return _transaction!;
+    PrismaClient factory(_i1.TransactionClient<PrismaClient> transaction) {
+      final client = PrismaClient(
+        engine: $engine,
+        datasources: $options.datasources,
+        datasourceUrl: $options.datasourceUrl,
+        errorFormat: $options.errorFormat,
+        log: $options.logEmitter.definition,
+      );
+      client.$options.logEmitter = $options.logEmitter;
+      client._transaction = transaction;
+
+      return client;
+    }
+
+    return _transaction = _i1.TransactionClient<PrismaClient>($engine, factory);
+  }
+
+  @override
+  get $engine => _engine ??= _i5.BinaryEngine(
+        schema:
+            '// This is your Prisma schema file,\n// learn more about it in the docs: https://pris.ly/d/prisma-schema\n\n// Looking for ways to speed up your queries, or scale easily with your serverless or edge functions?\n// Try Prisma Accelerate: https://pris.ly/cli/accelerate-init\n\ngenerator client {\n  provider = "dart run orm"\n  output   = "../lib/generated/prisma_client"\n}\n\ndatasource db {\n  provider = "postgresql"\n  url      = env("DATABASE_URL")\n}\n\nmodel User {\n  id Int @id @default(autoincrement())\n}\n\n// model Contact {\n//   id    Int     @id @default(autoincrement())\n//   User User[]\n// }\n',
+        datasources: const {
+          'db': _i1.Datasource(
+            _i1.DatasourceType.environment,
+            'DATABASE_URL',
+          )
+        },
+        options: $options,
+      );
+
+  @override
+  get $datamodel => datamodel;
+
+  UserDelegate get user => UserDelegate._(this);
+}
